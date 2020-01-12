346396794406caf4efedb6dad56d15bc18c73503
Kamil <kamafotoclub@gmail.com>
2020-01-12 13:36:33 +0500

    added worked solution without submition

---
+++ leetcode-7-easy.py
@@ -0,0 +1,25 @@
+def reverse(x):
+    """
+    :type x: int
+    :rtype: int
+    """
+    if x > 2 ** 31 - 1 or x < 2 ** 31 * -1:
+        return 0
+    else:
+        old_int_str = str(x)
+        new_int_str = ""
+        minus = False
+
+        if old_int_str[0] == '-':
+            minus = True
+            old_int_str = old_int_str[1:]
+
+        for i in range(len(old_int_str) - 1, -1, -1):
+            new_int_str += old_int_str[i]
+        new_x = int(new_int_str)
+        if minus:
+            new_x *= -1
+
+        return new_x
+
+print(reverse(-123))
