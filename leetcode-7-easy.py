96f4d64e282531eaea9519fbc252b1b2ef21f84f
Kamil <kamafotoclub@gmail.com>
2020-01-12 14:59:04 +0500

    added worked solution after fail in submition

--- leetcode-7-easy.py
+++ leetcode-7-easy.py
@@ -3,7 +3,7 @@ def reverse(x):
     :type x: int
     :rtype: int
     """
-    if x > 2 ** 31 - 1 or x < 2 ** 31 * -1:
+    if x > (2 ** 31 - 1) or x < (2 ** 31) * (-1):
         return 0
     else:
         old_int_str = str(x)
@@ -17,9 +17,11 @@ def reverse(x):
         for i in range(len(old_int_str) - 1, -1, -1):
             new_int_str += old_int_str[i]
         new_x = int(new_int_str)
+        if new_x >= 2 ** 31 - 1:
+            return 0
         if minus:
             new_x *= -1
 
         return new_x
 
-print(reverse(-123))
+
